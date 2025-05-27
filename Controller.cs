9f8040e4ad07ddc64f459e7c75c379395b5c3bd6
Aaron <aaronmadar2655@gmail.com>
2025-05-27 16:41:55 +0300

    feature 7

---
+++ Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseCommando
+{
+    internal class Controller
+    {
+        public void SayName(Commando commando, string Rank)
+        {
+            if (Rank.ToUpper() == "GENERAL")
+            {
+                Console.WriteLine(commando.GetName());
+
+            }
+
+            else if (Rank.ToUpper() == "Colonel")
+            {
+                Console.WriteLine(commando.CodeName);
+            }
+
+            else
+            {
+                Console.WriteLine("Private Information ");
+
+            }
+        }
+
+
+            
+        
+
+
+    }
+}
