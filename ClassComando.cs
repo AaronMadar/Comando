0d86a3034f80f95da5ce86d9d65c628bc470e509
Aaron <aaronmadar2655@gmail.com>
2025-05-27 10:35:54 +0300

    Class Commando

---
+++ ClassComando.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+
+namespace ExerciseCommando
+{
+
+
+	public class Commando
+	{
+		string Name;
+		string CodeName;
+		string[] Arms = new string[5];
+		string Status;
+
+
+		public Comando(string name, string codename, string status, string arms[5])
+		{
+			Name = name;
+			CodeName = codename;
+			Status = status;
+			Arms = arms;
+
+
+
+		}
+
+		static void Walk()
+		{
+			Status = "Walking";
+			Console.WriteLine($"{CodeName} is Walking right now ! ")
+		}
+
+        static void Hide()
+        {
+            Status = "Hidding";
+            Console.WriteLine($"{CodeName} is Hidding right now ! ")
+
+        }
+
+        static void Attack()
+        {
+            Status = "Attacking";
+            Console.WriteLine($"{CodeName} is Attacking right now ! ")
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
